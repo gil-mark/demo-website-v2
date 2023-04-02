0af7cf272503e2a811f13ebcba0b03ce90d9b759
gmark13 <gmark131@gmail.com>
2023-04-02 15:05:47 +0000

    more styling improvements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -27,6 +27,10 @@ TRIPS = [
 @app.route("/")
 def hello_world():
   return render_template('home.html', trips=TRIPS)
+
+@app.route("/api/trips")
+def list_trips():
+  return jsonify(TRIPS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
