9cf879b0f5466be73a1b10a5c9a3644f86b23fab
gil-mark <gmark131@gmail.com>
2023-05-02 18:40:53 +0000

    remove old trips list

--- app.py
+++ app.py
@@ -3,28 +3,6 @@ from database import load_trips_from_db
 
 app = Flask(__name__)
 
-TRIPS = [
-  {
-    'id': 1,
-    'title': 'Annapurna Circuit',
-    'country': 'Nepal',
-    'cost': 5000
-  },
-  {
-    'id': 2,
-    'title': 'England Bike Tour',
-    'country': 'United Kingdom',
-    'cost': 5000
-  },
-  {
-    'id': 3,
-    'title': 'Portugal Digital Nomad',
-    'country': 'Portugal',
-    'cost': 3000
-  }
-]
-
-
 @app.route("/")
 def hello_world():
   trips = load_trips_from_db()
